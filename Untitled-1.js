65ef6d6b8ffe342c917f7d29bd9f5543b35db9d2
priyanka <1984royalrajput@gmail.com>
2024-03-13 13:08:40 +0530

    issue fix

---
+++ Untitled-1.js
@@ -0,0 +1,36 @@
+var str = "";
+let num = 8;
+let abc = true;
+ const index = {sncsk:2, kjaa} ;
+ const array = [index, "baba", 1, true];
+array[0] 
+
+function my_fun(){
+    return true;
+}
+
+2 === 2
+11 != 9
+
+if (3 % 2 !== 0) {
+    true
+}
+
+else if(str) {
+
+}
+
+
+else {
+    false
+}
+
+for(let i= 0; i <= 10;   i++ ) {
+    console.log(i);
+}
+
+const ac = ["a","b","c"];
+
+for(let i=0; i <= ac.length -1; i++) {
+    console.log(ac[i]);
+}
